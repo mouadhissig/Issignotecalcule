f7c97d1a755ee452c95fc4bde60c45bde8359620
Toumi mouadh <toumimouadh@issig.u-gabes.tn>
2025-02-16 10:37:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const subjectsBySemester = {
     1: [
         { name: "Anatomie-physiologie 1", coeff: 2.5, credits: 5, isAnatomie: true },
         { name: "Déontologie-Ethique", coeff: 1, credits: 2 },
-        { name: "soin dan la Communauté", coeff: 1, credits: 2 },
+        { name: "soins dans la Communauté (1)", coeff: 1, credits: 2 },
         { name: "démarche de soins", coeff: 1.5, credits: 3 },
-        { name: "microbiologie", coeff: 1, credits: 2 },
+        { name: "microbiologie, parasitologie et immunité", coeff: 1, credits: 2 },
         { name: "Introduction à la Discipline infirmière", coeff: 1.5, credits: 3 },
         { name: "Psychologie du développement social", coeff: 1, credits: 2 }, // 2 notes
         { name: "Techniques infirmières 1", coeff: 1.5, credits: 3, singleNote: true, controlThreshold: 8 },
@@ -17,7 +17,7 @@ const subjectsBySemester = {
         { name: "Philosophie des sciences", coeff: 1, credits: 2, noControl: true },
         { name: "Anatomie-physiologie 2", coeff: 1.5, credits: 3, isAnatomie: true },
         { name: "Pharmacologie 1", coeff: 1, credits: 2 },
-        { name: "Soins Mère et nouveau né", coeff: 1, credits: 2 },
+        { name: "Soins à la Mère et nouveau né", coeff: 1, credits: 2 },
         { name: "Hygiène et environnement", coeff: 1, credits: 2 },
         { name: "Relation d'aide", coeff: 1, credits: 2 },
         { name: "Techniques infirmières 2", coeff: 1, credits: 2, singleNote: true, controlThreshold: 8 },
